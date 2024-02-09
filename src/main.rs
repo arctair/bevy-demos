50307c751250510813e419548d9548c491a56e86
tyler <tyler@cruftbusters.com>
2024-02-09 10:48:22 -0800

    camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use bevy::DefaultPlugins;
-use bevy::prelude::App;
+use bevy::prelude::{App, Camera2dBundle, Commands, default, OrthographicProjection, Startup};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup_camera)
         .run();
+}
+
+fn startup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scale: 1. / 1.,
+            ..default()
+        },
+        ..default()
+    });
 }
