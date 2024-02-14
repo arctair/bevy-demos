3774ac79aca63b957259789d93357e9f5fc09546
tyler <tyler@cruftbusters.com>
2024-02-14 12:11:22 -0800

    decouple center/size from id

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -9,57 +9,36 @@ impl QuadTree {
     pub fn new() -> QuadTree {
         QuadTree {
             nodes: vec![
-                QuadTreeNode::new(
-                    Vec2::new(0.5, 0.5),
-                    Vec2::new(1., 1.),
-                ),
-                QuadTreeNode::new(
-                    Vec2::new(0.25, 0.25),
-                    Vec2::new(0.5, 0.5),
-                ),
-                QuadTreeNode::new(
-                    Vec2::new(0.75, 0.25),
-                    Vec2::new(0.5, 0.5),
-                ),
-                QuadTreeNode::new(
-                    Vec2::new(0.25, 0.75),
-                    Vec2::new(0.5, 0.5),
-                ),
-                QuadTreeNode::new(
-                    Vec2::new(0.75, 0.75),
-                    Vec2::new(0.5, 0.5),
-                ),
+                QuadTreeNode::new(0b0, 0b0, 0),
             ]
         }
     }
 
-    pub fn leafs(&self) -> impl Iterator<Item=&QuadTreeNode> {
-        self.nodes[..1].into_iter()
+    pub fn leafs(&self) -> impl Iterator<Item=QuadTreeNode> + '_ {
+        self.nodes.iter().map(|q| q.clone())
     }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct QuadTreeNode {
-    center: Vec2,
-    size: Vec2,
+    x: i32,
+    y: i32,
+    depth: i32,
 }
 
 impl QuadTreeNode {
-    pub fn new(center: Vec2, size: Vec2) -> QuadTreeNode {
-        QuadTreeNode { center, size }
+    pub fn new(x: i32, y: i32, depth: i32) -> QuadTreeNode {
+        QuadTreeNode { x, y, depth }
     }
-    pub fn id(&self) -> QuadTreeNodeId { QuadTreeNodeId }
 
-    pub fn center(&self) -> Vec2 { self.center }
+    pub fn center(&self) -> Vec2 {
+        self.size() * Vec2::new(
+            0.5 + self.x as f32,
+            0.5 + self.y as f32,
+        )
+    }
 
-    pub fn size(&self) -> Vec2 { self.size }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct QuadTreeNodeId;
-
-impl QuadTreeNodeId {
-    pub fn new(x: i32, y: i32, level: i32) -> QuadTreeNodeId {
-        QuadTreeNodeId
+    pub fn size(&self) -> Vec2 {
+        Vec2::new(1.0, 1.0) / 2_f32.powf(self.depth as f32)
     }
 }
--- tests/quadtree.rs
+++ tests/quadtree.rs
@@ -1,16 +1,13 @@
 use bevy::prelude::Vec2;
-use quadtree_demo::quadtree::{QuadTree, QuadTreeNode, QuadTreeNodeId};
+use quadtree_demo::quadtree::{QuadTree, QuadTreeNode};
 
 #[test]
 fn new_root() {
     let quadtree = QuadTree::new();
 
-    let actual: Vec<QuadTreeNode> = quadtree.leafs().map(QuadTreeNode::clone).collect();
+    let actual: Vec<QuadTreeNode> = quadtree.leafs().collect();
     let expected = vec![
-        QuadTreeNode::new(
-            Vec2::new(0.5, 0.5),
-            Vec2::new(1.0, 1.0),
-        ),
+        QuadTreeNode::new(0b0, 0b0, 0),
     ];
 
     assert_eq!(actual, expected);
@@ -18,10 +15,27 @@ fn new_root() {
 
 #[test]
 fn id_root() {
-    let quadtree = QuadTree::new();
-
-    let actual: Option<QuadTreeNodeId> = quadtree.leafs().next().map(QuadTreeNode::id);
-    let expected = Some(QuadTreeNodeId::new(0b0, 0b0, 0));
-
-    assert_eq!(actual, expected);
+    let id = QuadTreeNode::new(0b0, 0b0, 0);
+    assert_eq!(id.center(), Vec2::new(0.5, 0.5));
+    assert_eq!(id.size(), Vec2::new(1.0, 1.0));
 }
+
+#[test]
+fn id_child() {
+    let id = QuadTreeNode::new(0b00, 0b00, 1);
+    assert_eq!(id.center(), Vec2::new(0.25, 0.25));
+    assert_eq!(id.size(), Vec2::new(0.5, 0.5));
+
+    let id = QuadTreeNode::new(0b01, 0b00, 1);
+    assert_eq!(id.center(), Vec2::new(0.75, 0.25));
+    assert_eq!(id.size(), Vec2::new(0.5, 0.5));
+
+    let id = QuadTreeNode::new(0b00, 0b01, 1);
+    assert_eq!(id.center(), Vec2::new(0.25, 0.75));
+    assert_eq!(id.size(), Vec2::new(0.5, 0.5));
+
+    let id = QuadTreeNode::new(0b01, 0b01, 1);
+    assert_eq!(id.center(), Vec2::new(0.75, 0.75));
+    assert_eq!(id.size(), Vec2::new(0.5, 0.5));
+}
+
